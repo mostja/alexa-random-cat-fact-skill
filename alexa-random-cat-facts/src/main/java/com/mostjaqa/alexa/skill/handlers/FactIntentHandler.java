dc13d45afdc8457ec9c366fb929a459c43b58295
Anastasiia Khalash <anastasiia.khalash@booxware.de>
2019-11-12 22:42:43 +0100

    Added calling catfact endpoint to get random fact

--- alexa-random-cat-facts/src/main/java/com/mostjaqa/alexa/skill/handlers/FactIntentHandler.java
+++ alexa-random-cat-facts/src/main/java/com/mostjaqa/alexa/skill/handlers/FactIntentHandler.java
@@ -3,6 +3,9 @@ package com.mostjaqa.alexa.skill.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 
 import java.util.Optional;
 
@@ -16,10 +19,18 @@ public class FactIntentHandler implements RequestHandler {
 
     public Optional<Response> handle(HandlerInput handlerInput) {
         String title = "Cat Facts";
-        String speechText = "I am Nastia, super whooper coder";
+        String speechText = "Nastia told me, that " + getSpeechText();
         return handlerInput.getResponseBuilder()
                 .withSpeech(speechText)
                 .withSimpleCard(title, speechText)
                 .build();
+    }
+
+    private String getSpeechText(){
+        Unirest.config().verifySsl(false);
+        HttpResponse<JsonNode> jsonResponse = Unirest.get("https://catfact.ninja/fact")
+                .header("accept", "application/json")
+                .asJson();
+        return jsonResponse.getBody().getObject().getString("fact");
     }
 }
